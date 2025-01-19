768cf868eba6f8e3c236adb4d4969d6cc5ebae15
JackJ30 <jackqjamison@proton.me>
2025-01-19 11:42:50 -0500

    themes, animaitons, emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,4 +1,5 @@
 (require 'org)
+(load-file ".config/emacs/sonokai_theme.el")
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
@@ -8,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(multiple-cursors diminish evil-nerd-commenter expand-region flycheck color-identifiers-mode consult doom-themes lsp-ui lsp-mode rainbow-delimiters yasnippet-snippets markdown-mode yasnippet no-littering vertico orderless marginalia vc-use-package miasma-theme kind-icon dracula-theme corfu)))
+   '(projectile embark-consult embark multiple-cursors diminish evil-nerd-commenter expand-region flycheck color-identifiers-mode consult doom-themes lsp-ui lsp-mode rainbow-delimiters yasnippet-snippets markdown-mode yasnippet no-littering vertico orderless marginalia vc-use-package miasma-theme kind-icon dracula-theme corfu)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
