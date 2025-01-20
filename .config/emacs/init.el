726577cdd1ea4a3ae72358c46fef5f5ed5ea1491
JackJ30 <jackqjamison@proton.me>
2025-01-20 15:26:20 -0500

    big improvements

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -3,16 +3,3 @@
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(projectile embark-consult embark multiple-cursors diminish evil-nerd-commenter expand-region flycheck color-identifiers-mode consult doom-themes lsp-ui lsp-mode rainbow-delimiters yasnippet-snippets markdown-mode yasnippet no-littering vertico orderless marginalia vc-use-package miasma-theme kind-icon dracula-theme corfu)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
