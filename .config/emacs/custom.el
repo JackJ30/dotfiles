35b51df6f6af4ce2baca711f09f714a80ac94d4e
JackJ30 <jackqjamison@proton.me>
2025-01-24 14:22:01 -0500

    cmake and better controls

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(nerd-icons-dired nerd-icons-corfu nerd-icons-completion nerd-icons dired-subtree yasnippet-snippets vertico rainbow-delimiters projectile orderless no-littering multiple-cursors marginalia magit lsp-ui glsl-mode flycheck expand-region evil-nerd-commenter embark-consult dracula-theme doom-themes diminish corfu color-identifiers-mode)))
+   '(cmake-font-lock cmake-mode nerd-icons-dired nerd-icons-corfu nerd-icons-completion nerd-icons dired-subtree yasnippet-snippets vertico rainbow-delimiters projectile orderless no-littering multiple-cursors marginalia magit lsp-ui glsl-mode flycheck expand-region evil-nerd-commenter embark-consult dracula-theme doom-themes diminish corfu color-identifiers-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
