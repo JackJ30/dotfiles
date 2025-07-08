38a407df9fe58161d5a42f771083a2a427eab450
Jack Jamison <53350163+JackJ30@users.noreply.github.com>
2025-07-08 10:57:07 -0400

    Update telescope.lua

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,16 +5,21 @@ return {
         'nvim-lua/plenary.nvim',
     },
     keys = function()
-        local lazy_telescope = function(builtin)
-            return function(...)
-                require('telescope.builtin')[builtin](...)
-            end
-        end
+		local lazy_telescope = function(builtin)
+			return function(...)
+				if builtin == 'find_files_custom' then
+					require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})
+				else
+					require('telescope.builtin')[builtin](...)
+				end
+			end
+		end
         return {
             { '<leader>fb', lazy_telescope('buffers'),                   desc = 'Find buffers' },
             { '<leader>fd', lazy_telescope('diagnostics'),               desc = 'Find diagnostics' },
-            { '<leader>ff', lazy_telescope('git_files'),                 desc = 'Find Git files' },
-            { '<leader>fF', lazy_telescope('find_files'),                desc = 'Find files' },
+            { '<leader>fF', lazy_telescope('git_files'),                 desc = 'Find Git files' },
+            { '<leader>fG', lazy_telescope('git_status'),                desc = 'Find Git status' },
+            { '<leader>ff', lazy_telescope('find_files_custom'),         desc = 'Find files' },
             { '<leader>fg', lazy_telescope('live_grep'),                 desc = 'Find files by content' },
             { '<leader>fh', lazy_telescope('help_tags'),                 desc = 'Find help tags' },
             { '<leader>fo', lazy_telescope('oldfiles'),                  desc = 'Find recently opened files' },
